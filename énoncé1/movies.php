4df3d3c13cb978aa3edd6afb240f1a88fc7e9da4
omayma hajjami <omayma.hajjami@gmail.com>
2020-05-29 10:01:09 +0000

    start server

--- énoncé1/movies.php
+++ énoncé1/movies.php
@@ -1,7 +1,7 @@
 <?php
 $NomFilm = $_GET["NomFilm"];
-$Acteur = $_GET ["Acteur"];
-$Date = $_GET ["Date"];
+$Acteur = $_GET["Acteur"];
+$Date = $_GET["Date"];
 ?>
 
 <!DOCTYPE html>
@@ -12,8 +12,8 @@ $Date = $_GET ["Date"];
     <title>Document</title>
 </head>
 <body>
-    <h2><strong> informations about <?php echo "$NomFilm"; ?>:</strong></h2>
-       <h4> <?php echo "§Acteur";?> started in the movie <?php echo "$NomFilm"; ?> which was released in year <?php echo "$Date"; ?><h4>
+    <h1><strong> informations about <?php echo "$NomFilm"; ?>:</strong></h1>
+       <h2> <?php echo "$Acteur";?> started in the movie <?php echo "$NomFilm"; ?> which was released in year <?php echo "$Date"; ?><h2>
      
 </body>
 </html>
