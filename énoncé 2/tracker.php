342d0fb564f329171348ef4c93566ac76d459847
omayma hajjami <omayma.hajjami@gmail.com>
2020-05-29 11:06:45 +0000

    function IMC

--- énoncé 2/tracker.php
+++ énoncé 2/tracker.php
@@ -1,15 +1,27 @@
 <?php
 
-$name= "omayma";
-$weightKg = 45;
-$heightCm = 160;
+// $name= "omayma";
+// $weightKg = 45;
+// $heightCm = 160;
 
-$heightM = $heightCm/100;
-$heightCarre = $heightM * $heightM;
+// $heightM = $heightCm/100;
+// $heightCarre = $heightM * $heightM;
 
-$IMC = $weightKg/$heightCarre;
+// $IMC = $weightKg/$heightCarre;
 
-echo "Bonjour $name , votre IMC est $IMC";
+// echo "Bonjour $name , votre IMC est $IMC";
+
+function IMC($name, $weightKg, $heightCm){
+    $heightM = $heightCm/100;
+    $heightCarre = $heightM * $heightM;
+    $IMC = $weightKg/$heightCarre;
+    $message = "Bonjour $name , votre IMC est $IMC";
+    return $message;
+
+}
+$message = IMC("Omayma",45,160);
+
+echo $message;
 
 ?>
 
