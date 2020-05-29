74e710c66bd86fcc02d832c0e6bfbe5910132070
omayma hajjami <omayma.hajjami@gmail.com>
2020-05-29 02:07:18 +0000

    structure html

--- énoncé1/movies.php
+++ énoncé1/movies.php
@@ -5,3 +5,16 @@ $Date = $_GET ["Date"];
 
 ?
 >
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2><strong> informations about <?php echo "$NomFilm"; ?>:</strong></h2>
+       <h4> <?php echo "§Acteur";?> started in the movie <?php echo "$NomFilm"; ?> which was released in year <?php echo "$Date"; ?><h4>
+     
+</body>
+</html>
