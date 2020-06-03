bde374db79d7f9241be5bbdd98647ebd31591f90
omayma hajjami <omayma.hajjami@gmail.com>
2020-06-03 03:38:49 +0100

    factoriel par la récursivité

--- énoncé 4/calcul.php
+++ énoncé 4/calcul.php
@@ -1,5 +1,6 @@
 <?php
 
+// Le calcul du factoriel 
 function fact(int $nbr){
 
     $fact=1;
@@ -7,7 +8,18 @@ function fact(int $nbr){
         $fact = $fact * $nbr;
         $nbr--;
     }
-    echo "le factoriel est : $fact";
+    echo "le factoriel est : $fact <br>";
 }fact(3);
 
+
+// Le calcul du factoriel avec la récursivité 
+function fct(int $num){
+
+    if($num === 0)
+        return 1;
+    else
+        return $num*(fct($num-1));
+}
+$result = fct(3);
+echo $result.'<br>'; 
 ?>
